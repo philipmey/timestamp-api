d678986866e07f226ca7ddb23b0b2a8829d5228a
Philip Mey <philipmey@gmail.com>
2016-08-18 10:12:34 +0000

    Static page completed. To Do: Routes returning valid or invalid dates

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 var express = require('express');
-
-var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    //console.log(req);
+  res.send('<html><body> \
+  <h1>API-Projects: Timestamp Microservice</h1> \
+  <h2>Functionality: </h2> \
+  <ol> \
+  <li>I can pass a string as a parameter and it will check to see whether that string contains either a unix timestamp or a natural language date. \
+  <ul> \
+  <li>If it does, it returns both the Unix timestamp and the natural language form of that date.</li> \
+  <li>If it does not, it returns null for those properties.</li> \
+  </ul> \
+  </li> \
+  </ol> \
+  <h2>Examples: </h2> \
+  <p>https://' + req.headers.host + '/December%2015,%202015<br/>https://' + req.headers.host + '/1450137600<br/>https://' + req.headers.host + '/December 15, 2015</p> \
+  <h2>Outputs: </h2> \
+  <p>Valid request: { "unix": 1450137600, "natural": "December 15, 2015" }<br/>Invalid request: { "unix": null, "natural": null }</p> \
+  </body></html>');
 });
 
 app.listen(8080, function () {
